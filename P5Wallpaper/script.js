62c4e873b0a58718f93d7680816c3b899cda3f0a
Joseph Curtis <92286532+jcurtis182@users.noreply.github.com>
2023-05-29 11:13:18 -0500

    added local weather functionality

--- P5Wallpaper/script.js
+++ P5Wallpaper/script.js
@@ -59,3 +59,42 @@ function checkDayNight() {      //city day/night cycle
         city.style.animation = "flashingCity 3s linear infinite";
     }
 }
+
+let lon, lat;
+let temperature = document.querySelector(".weather-temp");
+let loc = document.querySelector(".weather-location");
+let icon = document.querySelector(".weather-icon");
+const kelvin = 273;
+
+window.addEventListener("load", () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        lon = position.coords.longitude;
+        lat = position.coords.latitude;
+    
+        // API ID
+        const api = "6d055e39ee237af35ca066f35474e9df";
+    
+        // API URL
+        const base =
+  `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&` +
+  `lon=${lon}&appid=6d055e39ee237af35ca066f35474e9df`;
+    
+        // Calling the API
+        fetch(base)
+          .then((response) => {
+            return response.json();
+          })
+          .then((data) => {
+            console.log(data);
+            temperature.textContent = 
+                Math.floor(data.main.temp - kelvin) + "°C";
+            loc.textContent = data.name + ", " + data.sys.country;
+            let icon1 = data.weather[0].icon;
+            icon.innerHTML = 
+                `<img src="assets/img/${icon1}.png" style= 'height:100px'/>`;
+          });
+      });
+    }
+  });
